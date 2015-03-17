2bed3106569c83e2b8994dec262627b9a3c80e46
Nico Ameghino <nicolas.ameghino@gmail.com>
2015-03-17 16:55:23 -0300

    Initial commit

--- SparkCon/ViewController.swift
+++ SparkCon/ViewController.swift
@@ -10,9 +10,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var button: UIButton!
+    var turnOn = true
+    let core = SparkCore(
+        identifier: GLB_CORE3_IDENTIFIER,
+        authToken: AuthorizationToken
+    )
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        button.addTarget(self, action: "hitIt:", forControlEvents: .TouchUpInside)
+    }
+    
+    func hitIt(sender: UIControl) {
+        let session = NSURLSession.sharedSession()
+        
+        let command = SparkCommand.SetPin(.Relay, 0, (turnOn ? 1 : 0))
+        turnOn = !turnOn
+        
+        core.sendCommand(command) {
+            (data, response, error) in
+            return
+        }
+        
+    }
+    
+    override func viewDidAppear(animated: Bool) {
     }
 
     override func didReceiveMemoryWarning() {
